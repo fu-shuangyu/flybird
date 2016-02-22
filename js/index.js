ef45bad2f34cfc7952e37a8e5a272f55098db80a
双钰 <654910915@qq.com>
2016-02-22 09:49:59 +0800

    响应式布局添加

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 window.onload = function(){
-	var niao = document.querySelector('.niao')
+	var niao = document.querySelector('.niao');
+	var sheed = 1;
+	var W = 375,H = 627;
+	//只有在鸟过柱子的时候才检测是否发生碰撞的
+	vs = false;
+	var r;
+	var canvas = document.querySelector('#canvas');
+	var ctx = canvas.getContext('2d');
 	var bird = {
 		x:167.5,
 		y:293.5,
@@ -8,14 +15,8 @@ window.onload = function(){
 	};
 	var guandao = [
 	{top:{x:375,y:0,w:80,h:300},bottom:{x:375,y:400,w:80,h:300}},
-	{top:{x:600,y:0,w:80,h:300},bottom:{x:600,y:400,w:80,h:300}},
+	{top:{x:650,y:0,w:80,h:300},bottom:{x:650,y:400,w:80,h:300}},
 	]
-	var sheed = 1;
-	var W = 375,H = 627;
-	//只有在鸟过柱子的时候才检测是否发生碰撞的
-	vs = false;
-	var r;
-	var ctx = document.querySelector('#canvas').getContext('2d');
 	// 检测矩形之间的碰撞
 	var recvsrec =  function(rect0,rect1){
 	  if (rect0.x >= rect1.x && rect0.x >= rect1.x + rect1.w) {
@@ -54,12 +55,12 @@ window.onload = function(){
 			  return;
 			}
 			if(z.top.x <= (-z.top.w)){
-				z.top.x = 375;
-				z.bottom.x = 375;
+				z.top.x = 475;
+				z.bottom.x = 475;
 
 				z.top.h = Math.random()*200+150;
-				z.bottom.y = z.top.h+120;
-				z.bottom.h = 627-z.top.h -120;
+				z.bottom.y = z.top.h+150;
+				z.bottom.h = 627-z.top.h -150;
 			}
 		};
 		//边界判断
@@ -85,5 +86,20 @@ window.onload = function(){
 	canvas.addEventListener('touchend',function(e){
 	   bird.y -= 40;
 	 },false)
+	var cw = document.documentElement.clientWidth;
+	var ch = document.documentElement.clientHeight;
+	if(cw <= 377){
+		canvas.style.width = cw + 'px';
+	}
+	window.onresize = function(){
+		cw = document.documentElement.clientWidth;
+		ch = document.documentElement.clientHeight;
+		if(cw <= 377){
+			canvas.style.width = cw + 'px';
+		}
+		if(cw > 377){
+			canvas.style.width = 377 + 'px';
+		}
+	}
 	
 }
